7d51c53faf3a4ff8c8bf12d67b146fbaf9a9fefc
Daldanos <adolfogsilveira@gmail.com>
2024-01-26 08:27:33 -0800

    Making changes

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+def uppercase(str):
+    g = ""
+    for ch in str:
+        if ord(ch) >= 97 and ord(ch) <= 122:
+            x = ord(ch) - 32
+            y = chr(x)
+            g = g + y
+        else:
+            g = g + ch
+    print(g)
