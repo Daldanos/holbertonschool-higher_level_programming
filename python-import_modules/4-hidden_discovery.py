7c655340b360098cbf98727ee40876be25c0bb30
Daldanos <adolfogsilveira@gmail.com>
2024-01-27 14:37:36 -0800

    Making changes

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
+
+    search = dir(hidden_4)
+    dont = "__"
+    j = len(search)
+    for i in range(0, j):
+        if dont not in search[i]:
+            print(search[i])
