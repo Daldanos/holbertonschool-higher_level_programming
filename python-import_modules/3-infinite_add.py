7f9d2dfee8cf7b13fbaeb2a52a26afeb6521fdf9
Daldanos <adolfogsilveira@gmail.com>
2024-01-27 14:01:11 -0800

    Making changes

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    n = len(sys.argv) - 1
+    infinite = 0
+
+    for i in range(n):
+        infinite += int(sys.argv[i + 1])
+    print("{}".format(infinite))
