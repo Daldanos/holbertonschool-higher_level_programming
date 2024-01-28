516769285a9c55ab3c1102999b9c91d1e05541da
Daldanos <adolfogsilveira@gmail.com>
2024-01-28 12:25:07 -0800

    Making changes

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def multiple_returns(sentence):
+    lenght = len(sentence)
+    first = sentence[0]
+    if lenght == 0:
+        devol = [0, None]
+        return devol
+    else:
+        devol = [lenght, first]
+        return devol
