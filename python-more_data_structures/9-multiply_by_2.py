36e79829455dcdc79c0dc55f823c608ee3168bf4
Daldanos <adolfogsilveira@gmail.com>
2024-02-01 18:20:08 -0800

    Making changes

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    multiplied = a_dictionary.copy()
+    multiplied.update(
+            (key, value * 2) for key, value in multiplied.items()
+            )
+    return multiplied
