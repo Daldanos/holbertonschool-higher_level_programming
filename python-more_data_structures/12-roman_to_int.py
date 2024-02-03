c67145153858ad2d6886e6cf63d54e618ed10999
Daldanos <adolfogsilveira@gmail.com>
2024-02-03 09:36:23 -0800

    Making changes

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not roman_string:
+        return 0
+
+    romandict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    k = 0
+    result = 0
+
+    for j in roman_string[::-1]:
+        i = romandict[j]
+        if i >= k:
+            result += i
+        else:
+            result -= i
+        k = i
+
+    return result
