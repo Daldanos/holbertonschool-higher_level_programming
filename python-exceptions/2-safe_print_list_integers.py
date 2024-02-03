a6fe5931b3d0ff80f726e0fbcfae04157f125446
Daldanos <adolfogsilveira@gmail.com>
2024-02-03 12:24:42 -0800

    Making changes

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    i = 0
+    suma = 0
 
-    for j in my_list:
+    for i in range(x):
         try:
-            print("{:d}".format(j), end="")
-            i += 1
+            if type(my_list[i]) is int:
+                print("{:d}".format(my_list[i]), end="")
+                suma += 1
         except (ValueError, TypeError):
             pass
-        if i == x:
-            break
     print()
-    return i
+    return suma
