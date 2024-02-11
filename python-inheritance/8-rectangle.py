4c5d4b68187dff10ef284c042e10f4c9fa46f730
Daldanos <adolfogsilveira@gmail.com>
2024-02-11 14:28:58 -0800

    Making changes

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,32 +1,16 @@
 #!/usr/bin/python3
 """
 
-Function to return if obj in class.
-
-"""
-
-
-class BaseGeometry:
-    """Empty class BaseGeometry"""
-    def area(self):
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Validates Value"""
-        if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
-
-
-"""
-
 Class Rectangle
 
 """
 
 
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
 class Rectangle(BaseGeometry):
+    """Rectangle definition"""
     def __init__(self, width, height):
         self.integer_validator("width", width)
         self.integer_validator("height", height)
