564eb126af17de0c382651c5bb0050337c2be25d
Daldanos <adolfogsilveira@gmail.com>
2024-02-11 13:34:56 -0800

    Making changes

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+
+Function to return if obj in class.
+
+"""
+
+
+class BaseGeometry:
+    """Empty class BaseGeometry"""
+    def area(self):
+        raise Exception("area() is not implemented")
