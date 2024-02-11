bcf9413df5c851092ff878465d85c3fec1675916
Daldanos <adolfogsilveira@gmail.com>
2024-02-11 15:53:00 -0800

    Making changes

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -18,10 +18,7 @@ class Rectangle(BaseGeometry):
         self.__height = height
 
     def __str__(self):
-        if self.__width == 0 or self.__height == 0:
-            return ""
-        return (((str(self.print_symbol) * self.__width + "\n") *
-                self.__height).rstrip())
+        return ("[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
 
     def area(self):
         return (self.__width * self.__height)
