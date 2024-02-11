a30c35bcbf08e6fe4ff05620b7e252a6d9556fab
Daldanos <adolfogsilveira@gmail.com>
2024-02-11 13:10:02 -0800

    Making changes

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -7,4 +7,12 @@ Function to check if is the same class.
 
 
 def is_same_class(obj, a_class):
+    """Check object-class.
+
+    Args:
+        obj: To check.
+        a_class: The class.
+    Returns:
+        True or false
+    """
     return type(obj) == a_class
