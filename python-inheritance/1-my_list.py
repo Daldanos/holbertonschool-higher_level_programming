d52879c9ac030a69623a0d495fb47ed4cb1f0d5c
Daldanos <adolfogsilveira@gmail.com>
2024-02-11 11:55:29 -0800

    Making changes

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+
+Function to sort a list.
+
+"""
+
+
+class MyList(list):
+    def print_sorted(self):
+        """making the sort"""
+        sorted_list = sorted(self)
+        print(sorted_list)
