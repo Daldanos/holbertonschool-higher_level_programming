ed81cd505783ae5bae3ee3209848c5654a2fd487
Daldanos <adolfogsilveira@gmail.com>
2024-02-11 17:39:00 -0800

    Making changes

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 """
 
-Class Rectangle
+JSON representation of an object
 
 """
 
 
+def to_json_string(my_obj):
+    """function to return a JSON of an object"""
+    return json.dumps(my_obj)
