152febcad9b70434875b793e4bd0cd6cc7b219c1
Daldanos <adolfogsilveira@gmail.com>
2024-02-11 16:08:43 -0800

    Making changes

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -8,6 +8,8 @@ Class Rectangle
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+Rectangle = __import__('9-rectangle').Rectangle
+
 
 class Square(Rectangle):
     def __init__(self, size):
