95c487f93b2f951da83e72a955fe4a1eca3f195d
Daldanos <adolfogsilveira@gmail.com>
2024-02-11 17:58:50 -0800

    Making changes

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
 """
 
-Class Rectangle
+JSON writing
 
 """
 
+import json
 
+
+def load_from_json_file(filename):
+    """write to a file"""
+    with open(filename, encoding="utf-8") as f:
+        data = json.load(f)
+    return data
