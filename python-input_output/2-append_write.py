5c60f6a71898f5ab40fd51059ac6d616c4ced20c
Daldanos <adolfogsilveira@gmail.com>
2024-02-11 17:32:19 -0800

    Making changes

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -6,3 +6,7 @@ Class Rectangle
 """
 
 
+def append_write(filename="", text=""):
+    """Append text to a file"""
+    with open(filename, mode="a", encoding="utf-8") as f:
+        return f.write(text)
