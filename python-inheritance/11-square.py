539a8164824b2465d62b4d32737a3b7c4000f818
Daldanos <adolfogsilveira@gmail.com>
2024-02-11 16:26:16 -0800

    Making changes

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+
+Class Rectangle
+
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square definition from Rectangle"""
+    def __init__(self, size):
+        super().__init__(size, size)
+
+    def __str__(self):
+        return "[Square] {:d}/{:d}".format(self.width, self.height)
