f5c93a50a442e3af652684f69580893764fddb97
Daldanos <adolfogsilveira@gmail.com>
2024-02-11 17:17:56 -0800

    Making changes

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -8,5 +8,5 @@ Function to read a file and print it
 
 def read_file(filename=""):
     """Open a file and print it"""
-    with open(filename, mode="r", encoding="utf-8") as file:
-        print(file.read(), end="")
+    with open(filename, encoding="utf-8") as f:
+        print(f.read())
