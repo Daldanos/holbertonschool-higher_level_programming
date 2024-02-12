4ee733a8e1a3ad755eb35042cb352525392dafe0
Daldanos <adolfogsilveira@gmail.com>
2024-02-11 17:50:10 -0800

    Making changes

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 """
 
-Class Rectangle
+JSON representation of an object
 
 """
 
+import json
 
+
+def from_json_string(my_str):
+    """function to return a JSON of an object"""
+    return json.loads(my_str)
