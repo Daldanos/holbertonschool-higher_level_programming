d4b0add11428620ee95e397be1ec5c6ee2247bf6
Daldanos <adolfogsilveira@gmail.com>
2024-02-16 14:55:04 -0800

    Making changes

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -10,6 +10,19 @@ class Rectangle(Base):
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """Initialize new Rectangle
+
+        Args:
+            width (int): Width
+            height (int): Height
+            x (int): Coordinate x
+            y (int): Coordinate y
+            id (int): Identity of Rectangle
+
+        Raises:
+            TypeError: If not INT
+            ValueError: If less than 0
+            TypeError: If not INT
+            VaLueError: If less than 0
         """
         self.__width = width
         self.__height = height
@@ -23,6 +36,7 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """Get width"""
         return self.__width
 
     @width.setter
@@ -35,6 +49,7 @@ class Rectangle(Base):
 
     @property
     def height(self):
+        """Get Height"""
         return self.__height
 
     @height.setter
@@ -44,3 +59,27 @@ class Rectangle(Base):
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
+
+    @property
+    def x(self):
+        """Get X coordinate"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+
+    @property
+    def y(self):
+        """Get y coordinate"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
