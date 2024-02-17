d56d9b23d0774779d72e4d6f44e926137aa92d26
Daldanos <adolfogsilveira@gmail.com>
2024-02-17 14:43:09 -0800

    Making changes

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-"""
-Define a Rectangle Class
-"""
+"""Define a Rectangle Class."""
 from models.base import Base
 
 
 class Rectangle(Base):
-    """Rectangle"""
+    """A Rectangle class."""
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """Initialize new Rectangle
+        """Initialize new Rectangle.
 
         Args:
             width (int): Width
@@ -32,7 +30,7 @@ class Rectangle(Base):
 
     @property
     def width(self):
-        """Get width"""
+        """Rectangle width."""
         return self.__width
 
     @width.setter
@@ -45,7 +43,7 @@ class Rectangle(Base):
 
     @property
     def height(self):
-        """Get Height"""
+        """Rectangle height."""
         return self.__height
 
     @height.setter
@@ -58,7 +56,7 @@ class Rectangle(Base):
 
     @property
     def x(self):
-        """Get X coordinate"""
+        """x coordinate."""
         return self.__x
 
     @x.setter
@@ -67,10 +65,11 @@ class Rectangle(Base):
             raise TypeError("x must be an integer")
         if value < 0:
             raise ValueError("x must be >= 0")
+        self.__x = value
 
     @property
     def y(self):
-        """Get y coordinate"""
+        """y coordinate."""
         return self.__y
 
     @y.setter
@@ -79,3 +78,24 @@ class Rectangle(Base):
             raise TypeError("y must be an integer")
         if value < 0:
             raise ValueError("y must be >= 0")
+        self.__y = value
+
+    def area(self):
+        """Area of the retangle"""
+        return (self.width * self.height)
+
+    def perimeter(self):
+        return ((self.__width + self.__height) * 2)
+
+    def __str__(self):
+        return ("[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
+                self.x, self.y, self.width, self.height))
+
+    def __repr__(self):
+        return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
+
+    def display(self):
+        """Prints rectangle with "#" Character"""
+        for i in range(self.__height):
+            print("#" * self.__width, end="")
+            print()
