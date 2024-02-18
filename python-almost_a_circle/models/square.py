7e1c63191d30ceede976d07e1bbefd870085f153
Daldanos <adolfogsilveira@gmail.com>
2024-02-18 13:35:07 -0800

    Making changes

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -6,4 +6,27 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     """Define a Square"""
 
-    def
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initialize a new Square.
+
+        Args:
+            size (int): Size
+            x (int): x coordinate
+            y (int): y coordinate
+            id (int): Id
+        """
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """Square size."""
+        return self.__width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        return ("[Square] ({}) {}/{} - {}".format(self.id,
+                self.x, self.y, self.width))
