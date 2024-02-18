32ac3e2dac629996c6429704fe5134530c9ef0f6
Daldanos <adolfogsilveira@gmail.com>
2024-02-18 13:50:04 -0800

    Making changes

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -30,3 +30,28 @@ class Square(Rectangle):
     def __str__(self):
         return ("[Square] ({}) {}/{} - {}".format(self.id,
                 self.x, self.y, self.width))
+
+    def update(self, *args, **kwargs):
+        """Update square"""
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.size = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
+    def to_dictionary(self):
+        """Dictionary of Rectangle"""
+        return {
+                "id": self.id,
+                "size": self.size,
+                "x": self.x,
+                "y": self.y
+                }
+
