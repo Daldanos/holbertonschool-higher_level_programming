1a83e0c9f57caedb531f8d6f16401148a73c11e3
Daldanos <adolfogsilveira@gmail.com>
2024-02-18 14:01:17 -0800

    Making changes

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -28,7 +28,7 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         """returns the JSON string representation of list_dictionaries"""
-        if list_dictionaries is None or len(list_dictionaries) == 0:
-            return []
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
         else:
             return json.dumps(list_dictionaries)
