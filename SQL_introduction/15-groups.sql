3f6c5c69757b894db7769890229955b7be699d3a
Daldanos <adolfogsilveira@gmail.com>
2024-03-13 14:35:11 -0700

    Making changes

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,3 +1,3 @@
 -- Number by score
-SELECT score, COUNT(*) AS `number`
-FROM second_table GROUP BY score ORDER BY `number` DESC;
+SELECT score, COUNT(*) number
+FROM second_table GROUP BY score ORDER BY COUNT(*) DESC;
